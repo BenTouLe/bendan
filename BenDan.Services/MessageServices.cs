8cad6ff6b8271ff92c23a6aa4231b0a1d68ffa15
admin@3gjn.com <admin@3gjn.com>
2018-01-04 17:32:42 +0800

    实现用户登录与注册

---
+++ BenDan.Services/MessageServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenDan.Services
+{
+    public static class MessageServices
+    {
+        public static Task SendEmailAsync(string email, string subject, string message)
+        {
+            // Plug in your email service
+            return Task.FromResult(0);
+        }
+
+        public static Task SendSmsAsync(string number, string message)
+        {
+            // Plug in your sms service
+            return Task.FromResult(0);
+        }
+    }
+}
--- BenDan.UI/Controllers/AccountController.cs
+++ BenDan.UI/Controllers/AccountController.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using BenDan.ApplicationCore.Entities;
+using BenDan.ApplicationCore.Models;
+using BenDan.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BenDan.UI.Controllers
 {
     public class AccountController : Controller
     {
+
+        public AccountController(
+             UserManager<User> userManager,
+             SignInManager<User> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public UserManager<User> UserManager { get; }
+        public SignInManager<User> SignInManager { get; }
+
         public IActionResult Index()
         {
             return View();
@@ -22,5 +39,81 @@ namespace BenDan.UI.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+              //  _logger.LogInformation("Logged in {userName}.", model.UserName);
+                return RedirectToLocal(returnUrl);
+            }
+            else
+            {
+               // _logger.LogWarning("Failed to log in {userName}.", model.UserName);
+                ModelState.AddModelError("", "用户名或密码错误");
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.UserName, Email = model.Email, CreateOn = DateTime.Now, LastTime = DateTime.Now };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                  //  _logger.LogInformation("User {userName} was created.", model.Email);
+                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                    await MessageServices.SendEmailAsync(model.Email, "Confirm your account",
+                        "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
+                    if (user.UserName.ToLower().Equals("admin"))
+                    {
+                        await UserManager.AddClaimAsync(user, new Claim("Admin", "Allowed"));
+                    }
+                    return RedirectToAction("Login");
+                }
+                AddErrors(result);
+            }
+            return View(model);
+        }
+
+
+        #region 辅助方法
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+                //_logger.LogWarning("Error in creating user: {error}", error.Description);
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        #endregion
+
     }
 }
